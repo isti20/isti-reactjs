d803b95ce8c56a35ac27a07ad66eb85fd7dea0ce
iis istiqomah <iisistiqomah20@gmail.com>
2023-03-08 15:33:30 +0700

    Create Navbar.jsx file

---
+++ src/component/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+function Navbar() {
+    return(
+        <div className="n-container">
+            <div className="n-left">
+                <div className="n-name"></div>
+                <span>Toggle</span>
+            </div>
+            <div className="n-right">
+                <div className="n-list">
+                    <ul>
+                        <li>Home</li>
+                        <li>Services</li>
+                        <li>Experience</li>
+                        <li>Portfolio</li>
+                        <li>About Me</li>
+                    </ul>
+                </div>
+                <button className="button">Contact</button>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
