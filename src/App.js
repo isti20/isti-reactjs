09e1c62063b185e0c0f3b0c43dad1c37bd860c27
iis istiqomah <iisistiqomah20@gmail.com>
2023-03-08 16:26:58 +0700

    Create Bio.jsx and import to App.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './component/Navbar/Navbar';
+import Bio from './component/Bio/Bio';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Bio />
     </div>
   );
 }
---
+++ src/component/Bio/Bio.jsx
@@ -0,0 +1,19 @@
+function Bio() {
+    return(
+        <div className="bio">
+            <div className="b-left">
+                <div className="b-name">
+                    <span>Hi! I am</span>
+                    <span>Iis Istiqomah</span>
+                    <span>Frontend Developer with experience
+                        in web designing and development
+                    </span>
+                </div>
+                <button className="b-button">Hire me</button>
+            </div>
+            <div className="b-right"></div>
+        </div>
+    );
+}
+
+export default Bio;
