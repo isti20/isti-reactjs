827e3eb9f269669e5fd1c18f574fc072fc127f4c
iis istiqomah <iisistiqomah20@gmail.com>
2023-03-09 12:58:03 +0700

    update Bio.jsx: import img

--- src/component/Bio/Bio.jsx
+++ src/component/Bio/Bio.jsx
@@ -2,6 +2,12 @@ import './Bio.css';
 import Github from '../../img/github.png';
 import Linkedin from '../../img/linkedin.png';
 import Instagram from '../../img/instagram.png';
+import Vector1 from '../../img/Vector1.png';
+import Vector2 from '../../img/Vector2.png';
+import Woman from '../../img/woman.png';
+import Thumbup from '../../img/thumbup.png';
+import Crown from '../../img/crown.png';
+import Glassesimoji from '../../img/glassesimoji.png';
 
 function Bio() {
     return(
@@ -21,7 +27,11 @@ function Bio() {
                     <img src={Instagram} alt="" />
                 </div>
             </div>
-            <div className="b-right"></div>
+            <div className="b-right">
+                <img src={Vector1} alt="" />
+                <img src={Vector2} alt="" />
+                <img src={Woman} alt="" />
+            </div>
         </div>
     );
 }
